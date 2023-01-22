7da62ed2294e1f036cb349c8e6027e44728c92af
CHAN Ho Long <roychanho@gmail.com>
2023-01-23 02:31:26 +0800

    update

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,114 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import Input from "./Input";
+import Button from "../UI/Button";
+
+const ExpenseForm = ({ onCancel, onSubmit, submitButtonLabel }) => {
+  const [inputValues, setInputValues] = useState({
+    amount: "",
+    date: "",
+    description: "",
+  });
+
+  //   const [amountValue, setAmountValue] = useState("");
+
+  //   function amountChangedHandler(enteredAmount) {
+  //     setAmountValue(enteredAmount);
+  //   }
+
+  function inputChangedHandler(inputIdentifier, enteredValue) {
+    setInputValues((curInputValues) => {
+      return {
+        ...curInputValues,
+        [inputIdentifier]: enteredValue,
+      };
+    });
+  }
+
+  function submitHandler() {
+    const expenseData = {
+      amount: +inputValues.amount, // plus converts this string to a number
+      date: new Date(inputValues.date),
+      description: inputValues.description,
+    };
+
+    onSubmit(expenseData);
+  }
+
+  return (
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            // onChangeText: amountChangedHandler,
+            // value: amountValue,
+            // bind for the inputChangedHandler inputIdentifier, this = inputIdentifier, inputIdentifier = 'amount'
+            onChangeText: inputChangedHandler.bind(this, "amount"),
+            value: inputValues.amount,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: inputChangedHandler.bind(this, "date"),
+            value: inputValues.date,
+          }}
+        />
+      </View>
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          //   autoCorrect: false,
+          //   autoCapitalize: "none",
+          onChangeText: inputChangedHandler.bind(this, "description"),
+          value: inputValues.description,
+        }}
+      />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+export default ExpenseForm;
+
+const styles = StyleSheet.create({
+  form: { marginTop: 40 },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginVertical: 24,
+    textAlign: "center",
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});
